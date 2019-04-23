7756db909ecd878213cbd706086ac8557ee0de8c
Hassaku-kun <masaki.tani526@gmail.com>
2019-04-23 13:36:42 +0900

    import konva

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
+import VueKonva from 'vue-konva';
 
 import App from './App';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.config.productionTip = false;
+
+Vue.use(VueKonva);
 
 /* eslint-disable no-new */
 new Vue({
